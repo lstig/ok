0aea37929968a89a47b7a204b485c4f4722ba964
Luke Stigdon <lstig@users.noreply.github.com>
2024-07-15 20:50:26 -0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net/http"
+)
+
+// options contains the configuration for the webserver
+type options struct {
+	path   string
+	listen string
+}
+
+func main() {
+	var opts options
+	flag.StringVar(&opts.path, "path", "/", "path to serve")
+	flag.StringVar(&opts.listen, "listen", ":8080", "the address to listen on")
+	flag.Parse()
+
+	http.HandleFunc(opts.path, func(w http.ResponseWriter, r *http.Request) {
+		if _, err := io.WriteString(w, "ok"); err != nil {
+			log.Fatal(err)
+		}
+	})
+	log.Fatal(http.ListenAndServe(opts.listen, nil))
+}
